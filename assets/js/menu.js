d96eeeedd68f22c32b05bf175e2ef22c9272f086
Weverton Costa <weverton.costa@thealfred.com.br>
2023-04-28 09:56:02 -0300

    variables add v01

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -12,11 +12,3 @@ $(document).ready(function(){
 		});
 	}); 
 });
-
-$('.scroll').on('click', function(e) {
-	e.preventDefault();
-	var hash = this.hash;
-	$('html, body').animate({
-		scrollTop: $(hash).offset().top
-	}, 1000);
-});
