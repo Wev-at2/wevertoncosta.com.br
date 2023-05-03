35963fcbead7ab8e3fd34fe930ced627350dfb1d
Weverton Costa <weverton.costa@thealfred.com.br>
2023-05-03 09:19:39 -0300

    wc-contact__form update v01

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -12,3 +12,11 @@ $(document).ready(function(){
 		});
 	}); 
 });
+
+$('.scroll').on('click', function(e) {
+	e.preventDefault();
+	var hash = this.hash;
+	$('html, body').animate({
+		scrollTop: $(hash).offset().top
+	}, 1000);
+});
