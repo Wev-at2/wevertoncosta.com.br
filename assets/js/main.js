3dbc81ac48152a03aaddf606711d343ccf4a23ac
Weverton <weverton.costa2011.wc@gmail.com>
2023-09-22 14:43:50 -0300

    feat(main): scripts added in mian.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
-// import toggleMenu from "./menu_mobile.js";
 import { typeWrite } from "./typeWrite.js";
+import { setupMenu } from "./menu_mobile.js";
+import './menu.js';
 
-// toggleMenu();
 typeWrite(document.querySelector(".typewriter"));
+setupMenu();
--- assets/js/menu_mobile.js
+++ assets/js/menu_mobile.js
@@ -1,17 +1,32 @@
-const btnMobile = document.getElementById('btn-mobile');
+export function setupMenu() {
+  const btnMobile = document.getElementById("btn-mobile");
 
-function toggleMenu(event) {
-  if (event.type === 'touchstart') event.preventDefault();
-  const nav = document.getElementById('nav');
-  nav.classList.toggle('active');
-  const active = nav.classList.contains('active');
-  event.currentTarget.setAttribute('aria-expanded', active);
-  if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-  } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  function toggleMenu(event) {
+    if (event.type === "touchstart") event.preventDefault();
+    const headerNav = document.getElementById("header-nav");
+    headerNav.classList.toggle("active");
+    const active = headerNav.classList.contains("active");
+    event.currentTarget.setAttribute("aria-expanded", active);
+    if (active) {
+      event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+    } else {
+      event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+    }
   }
-}
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+  function closeMenuOnOptionSelect() {
+    const headerNav = document.getElementById("header-nav");
+    headerNav.classList.remove("active");
+    btnMobile.setAttribute("aria-expanded", false);
+    btnMobile.setAttribute("aria-label", "Abrir Menu");
+  }
+
+  const menuItems = document.querySelectorAll(".wc-header__menu--nav_item a");
+
+  menuItems.forEach((menuItem) => {
+    menuItem.addEventListener("click", closeMenuOnOptionSelect);
+  });
+
+  btnMobile.addEventListener("click", toggleMenu);
+  btnMobile.addEventListener("touchstart", toggleMenu);
+}
