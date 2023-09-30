ff314049b1eba5d550555314f16c4bdd9fa80daf
Weverton <weverton.costa2011.wc@gmail.com>
2023-09-30 03:26:57 -0300

    feat(script): slide adde in projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 import { typeWrite } from "./typeWrite.js";
 import { setupMenu } from "./menu_mobile.js";
 import './menu.js';
+import './slick_slide.js';
 
 typeWrite(document.querySelector(".typewriter"));
 setupMenu();
---
+++ assets/js/slick_slide.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  $(".wc-projects__items").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    dots: true,
+    infinite: true,
+    centerMode: false,
+    speed: 300,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
