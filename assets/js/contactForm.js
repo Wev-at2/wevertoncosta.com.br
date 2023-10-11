54f72337df57f8c3dbd33dd898b10344741b265d
Weverton <weverton.costa2011.wc@gmail.com>
2023-10-11 20:14:26 -0300

    feat(contactForm): new file scripts added

---
+++ assets/js/contactForm.js
@@ -0,0 +1,20 @@
+// export function phoneFormat(input) {
+//   var numeros = input.value.replace(/\D/g, '');
+
+//   if (numeros.length === 11) {
+//       input.value = numeros.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+//   } else {
+//       input.value = numeros;
+//   }
+// }
+// fetch("https://api.apispreadsheets.com/data/S95RWRLe0l6lejGg/", {
+// 	method: "POST",
+// 	body: JSON.stringify({"data": {"Nome":"","E-mail":"","Mensagem":""}}),
+// }).then(res =>{
+// 	if (res.status === 201){
+// 		// SUCCESS
+// 	}
+// 	else{
+// 		// ERROR
+// 	}
+// })
