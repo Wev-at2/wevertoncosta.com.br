c14e2f6bf04de99b929acf6e85c2cc864377eb38
Weverton <weverton.costa2011.wc@gmail.com>
2023-10-14 11:23:11 -0300

    feat(main): scripts updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,16 @@
 // main.js
-import { typeWrite } from "./typeWrite.js";
-import { setupMenu } from "./menu_mobile.js";
-import { phoneNumberUtils } from "./contactForm.js";
-import { setupFormSubmit } from "./contactForm.js"; 
-import "./menu.js";
 import "./slick_slide.js";
+import "./menu.js";
+import { setupMenu } from "./menu_mobile.js";
+import { typeWrite } from "./typeWrite.js";
+// import { phoneNumberUtils } from "./contactForm.js";
+// import { setupFormSubmit } from "./contactForm.js"; 
 
 typeWrite(document.querySelector(".typewriter"));
 setupMenu();
 
-const phoneInput = document.getElementById("wc-contact__phone");
-
-phoneInput.addEventListener("input", function () {
-  phoneNumberUtils.format(this);
-});
-setupFormSubmit();
+// const phoneInput = document.getElementById("wc-contact__phone");
+// phoneInput.addEventListener("input", function () {
+//   phoneNumberUtils.format(this);
+// });
+// setupFormSubmit();
