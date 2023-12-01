a687bf3e69b371b021ffbe9d5c167ec0738a7cc0
Weverton <weverton.costa2011.wc@gmail.com>
2023-11-30 21:29:13 -0300

    feat(slick): autoplay removed

--- assets/js/slick_slide.js
+++ assets/js/slick_slide.js
@@ -2,20 +2,17 @@ $(document).ready(function () {
   $(".wc-projects__items").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 9000,
     dots: true,
     infinite: true,
-    centerMode: false,
     speed: 300,
+    // autoplay: true,
+    // autoplaySpeed: 9000,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-          infinite: true,
-          dots: true,
         },
       },
       {
