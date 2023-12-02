5dd9fcfaa45218c5cbec6feda4a611bc5f864421
Weverton <weverton.costa2011.wc@gmail.com>
2023-12-01 21:00:16 -0300

    perf: main scripts optimized v01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,13 @@
-// main.js
 import "./slick_slide.js";
 import "./menu.js";
 import { setupMenu } from "./menu_mobile.js";
 import { typeWrite } from "./typeWrite.js";
-// import { phoneNumberUtils } from "./contactForm.js";
-// import { setupFormSubmit } from "./contactForm.js"; 
 
 typeWrite(document.querySelector(".typewriter"));
 setupMenu();
 
+// import { phoneNumberUtils } from "./contactForm.js";
+// import { setupFormSubmit } from "./contactForm.js"; 
 // const phoneInput = document.getElementById("wc-contact__phone");
 // phoneInput.addEventListener("input", function () {
 //   phoneNumberUtils.format(this);
