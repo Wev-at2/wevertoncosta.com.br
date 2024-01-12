f4c15b2e442df6046f2d6c4a4ecae2eb11ee7206
Weverton <weverton.costa2011.wc@gmail.com>
2024-01-12 18:22:51 -0300

    feat(menu.js): beautify code and fixed header apply

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,22 +1,40 @@
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-		e.preventDefault();
+$(document).ready(function () {
+  $('a[href^="#"]').on("click", function (e) {
+    e.preventDefault();
 
-		var target = this.hash;
-		var $target = $(target);
+    var target = this.hash;
+    var $target = $(target);
 
-		$('html, body').stop().animate({
-			'scrollTop': $target.offset().top
-		}, 900, 'swing', function () {
-			window.location.hash = target;
-		});
-	}); 
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top,
+        },
+        900,
+        "swing",
+        function () {
+          window.location.hash = target;
+        }
+      );
+  });
 });
 
-$('.scroll').on('click', function(e) {
-	e.preventDefault();
-	var hash = this.hash;
-	$('html, body').animate({
-		scrollTop: $(hash).offset().top
-	}, 1000);
+$(".scroll").on("click", function (e) {
+  e.preventDefault();
+  var hash = this.hash;
+  $("html, body").animate(
+    {
+      scrollTop: $(hash).offset().top,
+    },
+    1000
+  );
+});
+
+$(window).scroll(function () {
+  var sticky = $(".sticky"),
+    scroll = $(window).scrollTop();
+
+  if (scroll >= 100) sticky.addClass("fixed");
+  else sticky.removeClass("fixed");
 });
