b3ba26498fc2ead02549be00162cf45d147f4dca
Weverton <weverton.costa2011.wc@gmail.com>
2024-01-17 18:45:54 -0300

    feat(slideSections): new updates

--- assets/js/slideSections.js
+++ assets/js/slideSections.js
@@ -1,7 +1,8 @@
 export function setupSlideEffect() {
   window.sr = ScrollReveal({ reset: true });
-  sr.reveal(".wc-main__banner", { duration: 300 });
-  sr.reveal("section", { duration: 2000 });
+  sr.reveal(".wc-main__banner", { duration: 2000 });
   sr.reveal(".wc-about *", { duration: 2000 });
+  sr.reveal(".wc-skills", { duration: 2000 });
   sr.reveal(".wc-experience *", { duration: 2000 });
+  sr.reveal(".wc-projects", { duration: 2000 });
 }
