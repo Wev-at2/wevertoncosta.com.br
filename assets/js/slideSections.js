ecbe1d3a523168863ee974af1e680f76a58b08f6
Weverton <weverton.costa2011.wc@gmail.com>
2024-01-17 21:53:02 -0300

    feat: index link updates

--- assets/js/slideSections.js
+++ assets/js/slideSections.js
@@ -1,8 +1,8 @@
 export function setupSlideEffect() {
   window.sr = ScrollReveal({ reset: true });
   sr.reveal(".wc-main__banner", { duration: 2000 });
-  sr.reveal(".wc-about *", { duration: 2000 });
-  sr.reveal(".wc-skills", { duration: 2000 });
-  sr.reveal(".wc-experience *", { duration: 2000 });
-  sr.reveal(".wc-projects", { duration: 2000 });
+  sr.reveal(".wc-about *", { duration: 1500 });
+  sr.reveal(".wc-skills", { duration: 1000 });
+  sr.reveal(".wc-experience *", { duration: 1000 });
+  sr.reveal(".wc-projects", { duration: 1500 });
 }
