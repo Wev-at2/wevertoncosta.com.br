dfb7940a7957d0327aad3095a78c45c7338be0b5
Weverton <weverton.costa2011.wc@gmail.com>
2024-02-19 17:28:41 -0300

    perf: slickSlide updates for IOS devices

--- assets/js/slideSections.js
+++ assets/js/slideSections.js
@@ -1,8 +1,15 @@
 export function setupSlideEffect() {
-  window.sr = ScrollReveal({ reset: true });
-  sr.reveal(".wc-main__banner", { duration: 2000 });
-  sr.reveal(".wc-about *", { duration: 1500 });
-  sr.reveal(".wc-skills", { duration: 1000 });
-  sr.reveal(".wc-experience *", { duration: 1000 });
-  sr.reveal(".wc-projects", { duration: 1500 });
+    window.sr = ScrollReveal({ reset: true });
+    sr.reveal(".wc-main__banner", {
+        duration: 2000,
+        distance: "50px",
+    });
+    sr.reveal(".wc-about", { duration: 3000 });
+    sr.reveal(".wc-skills li", {
+        duration: 1500,
+        distance: "50px",
+        interval: 200,
+    });
+    sr.reveal(".wc-experience *", { duration: 1000 });
+    // sr.reveal(".wc-projects", { duration: 1500 });
 }
