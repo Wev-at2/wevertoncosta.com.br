efd77b3476804100dd7fa262a18591343df9edd6
Weverton <weverton.costa2011.wc@gmail.com>
2024-09-20 15:46:11 -0300

    feat: projects items updates

--- assets/js/slick_slide.js
+++ assets/js/slick_slide.js
@@ -3,17 +3,19 @@ $(document).ready(function () {
     $(".wc-projects__items").slick({
         slidesToShow: 3,
         slidesToScroll: 1,
-        centerMode: true,
         dots: true,
-        centerPadding: "16px",
         speed: 900,
         infinite: true,
+        centerMode: true,
+        centerPadding: "-10px",
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 1,
                     slidesToScroll: 1,
+                    centerMode: true,
+                    centerPadding: "-10px",
                 },
             },
             {
@@ -21,6 +23,8 @@ $(document).ready(function () {
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
+                    centerMode: true,
+                    centerPadding: "-10px",
                 },
             },
         ],
