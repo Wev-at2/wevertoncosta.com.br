7c50499c35deb48c9e9a71807c7fdd03742e4866
Weverton Costa <weverton.costa2011.wc@gmail.com>
2025-03-01 03:49:11 -0300

    feat: swiper and scripts updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,12 @@ import { menuScroll } from "./menu_scroll.js";
 import { setupMenu } from "./menu_mobile.js";
 import { setupSlideEffect } from "./slideSections.js";
 import { typeWrite } from "./typeWrite.js";
+import { swiperInit } from "./swiper_init.js";
 
 menuScroll();
 setupMenu();
 setupSlideEffect();
+swiperInit();
 const typewriterElement = document.querySelector(".typewriter");
 if (typewriterElement) {
   typeWrite(typewriterElement);
--- assets/js/swiper_init.js
+++ assets/js/swiper_init.js
@@ -1,42 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const swiper = new Swiper('.wc-projects__items', {
-    // Configurações do Swiper
-    effect: "coverflow",
-    // grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    autoplay: {
-      delay: 7000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 1,
-        spaceBetween: 20,
+export function swiperInit() {
+  document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.wc-projects__items', {
+      loop: true,
+      effect: "coverflow",
+      centeredSlides: true,
+      centeredSlidesBounds: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 20,
+        modifier: 1,
+        slideShadows: true,
       },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 40,
+      autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
       },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 50,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
       },
-    },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+    });
   });
-});
+
+}
