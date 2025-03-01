e1b44b988a15ff072df43cc41cac6c296367c9b1
Weverton dos Santos Souza Costa <82066256+Wev-at2@users.noreply.github.com>
2025-03-01 02:39:51 -0300

    feat: swiper test 6

--- assets/js/menu_scroll.js
+++ assets/js/menu_scroll.js
@@ -6,18 +6,20 @@ export function menuScroll() {
             var target = this.hash;
             var $target = $(target);
 
-            $("html, body")
-                .stop()
-                .animate(
-                    {
-                        scrollTop: $target.offset().top,
-                    },
-                    900,
-                    "swing",
-                    function () {
-                        window.location.hash = target;
-                    }
-                );
+            if ($target.length) {
+                $("html, body")
+                    .stop()
+                    .animate(
+                        {
+                            scrollTop: $target.offset().top,
+                        },
+                        900,
+                        "swing",
+                        function () {
+                            window.location.hash = target;
+                        }
+                    );
+            }
         });
     });
 
